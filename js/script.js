f42f6193486f487a86e2a9cbdba84f0293617dcb
Mateus Wenzler <mateuswenzler@hotmail.com>
2023-03-08 15:33:07 -0300

    Adicionando JavaScript

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//Ativar Links do menu
+const links = document.querySelectorAll('.header-menu a');
+function ativarLink(link){
+  const url = window.location.href;
+  const href = link.href;
+  if(url.includes(href)){
+      link.classList.add("ativo");
+  }
+}
+links.forEach(ativarLink);
+
+//Ativar Itens do Orçamento
+const parametros = new URLSearchParams(location.search);
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if(elemento){
+  elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
